8113afacf5a1c62067f5de92fc5b5b9921cf8f3a
Seymen Akcan <153684324+seymenakcn@users.noreply.github.com>
2026-02-08 01:31:17 +0200

    Update honeypot.go
    Added logging functionality to record connection attempts.

--- honeypot.go
+++ honeypot.go
@@ -4,27 +4,68 @@ import (
 	"bufio"
 	"fmt"
 	"net"
+	"os"
+	"strings"
+	"time"
 )
+
+func writeLog(line string) {
+	file, err := os.OpenFile(
+		"honeypot.log",
+		os.O_APPEND|os.O_CREATE|os.O_WRONLY,
+		0644,
+	)
+	if err != nil {
+		return
+	}
+	defer file.Close()
+
+	file.WriteString(line + "\n")
+}
+
+func logAndPrint(message string) {
+	fmt.Println(message)
+	writeLog(message)
+}
 
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
 
-	fmt.Println("Handling connection from", conn.RemoteAddr())
+	remote := conn.RemoteAddr().String()
+	logAndPrint("Handling connection from" + remote)
 
 	conn.Write([]byte("SSH-2.0-OpenSSH_8.2p1 Ubuntu-4ubuntu0.5\r\n"))
 
 	reader := bufio.NewReader(conn)
-	input, err := reader.ReadString('\n')
+
+	conn.Write([]byte("login: "))
+	username, err := reader.ReadString('\n')
 	if err != nil {
-		fmt.Println("Read error:", err)
 		return
 	}
 
-	fmt.Println("Recieved", input)
+	conn.Write([]byte("password: "))
+	password, err := reader.ReadString('\n')
+	if err != nil {
+		return
+	}
+
+	timestamp := time.Now().Format("2006-01-02 15:04:05")
+
+	logLine := fmt.Sprintf(
+		"[%s] %s | login=%s password=%s",
+		timestamp,
+		remote,
+		strings.TrimSpace(username),
+		strings.TrimSpace(password),
+	)
+	logAndPrint(logLine)
+
+	conn.Write([]byte("Authentication failed.\r\n"))
+
 }
 
 func main() {
-	fmt.Println("Honeypot Starting...")
 
 	listener, err := net.Listen("tcp", ":2222")
 	if err != nil {
@@ -34,7 +75,7 @@ func main() {
 
 	defer listener.Close()
 
-	fmt.Println("Listening on port 2222")
+	fmt.Println("Honeypot Listening on port 2222")
 
 	for {
 		conn, err := listener.Accept()
@@ -42,7 +83,6 @@ func main() {
 			continue
 		}
 
-		fmt.Println("New Connection from", conn.RemoteAddr())
 		go handleConnection(conn)
 	}
 }
